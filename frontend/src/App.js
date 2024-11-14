af679eeccc88b2bf04b82bbebb7b3d68705c6af5
Naresh Gupta <nareshgupta0899@gmail.com>
2024-11-14 12:24:51 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Test from "./component/Test";
+
+function App() {
+  return (
+    <div className="App">
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div className='text-violet-800'>Test</div>
+  )
+}
+
+export default Test
