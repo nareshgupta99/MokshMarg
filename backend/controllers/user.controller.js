965b50cc015196bb393e153d34465f98f92b05e1
Naresh Gupta <nareshgupta0899@gmail.com>
2024-12-06 13:58:59 +0530

    reset password updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,53 +3,33 @@ const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
 const crypto = require('crypto');
 const emailSend = require('../config/email.config')
-const User=require("../models/user.model") 
-const ApiError=require("../utils/ApiError")
+const User = require("../models/user.model")
+const ApiError = require("../utils/ApiError")
 
 
 
 dotenv.config();
 
-const updatePassword = (async (req, res) => {
-    const { currentPassword, newPassword } = req.body;
-    const { email } = getDecodedToken();
+const registerUser = (async (req, res) => {
+    const { email, password, confirmPassword, name } = req.body;
+    if (password != confirmPassword) {
+        throw new ApiError(401, "password and confirm password must be match");
+    }
+    const hash = await encryptPassword(password);
+
     const user = await User.findOne({ email });
-    console.log(user);
-    if (!user) {
-        res.status(500).json({ error: "Internal server error " });
+    if (user) {
+        throw new ApiError(401, "user already registerd");
     }
-    const isMatch = await bcrypt.compare(currentPassword, user.password);
-    if (isMatch) {
-        const newHashedPassword = await encryptPassword(newPassword);
-        user.password = newHashedPassword;
-        await user.save();
-        res.status(200).json({ message: "Password updated sucessfully" });
 
-    } else {
-        res.status(400).json({ error: "wrong  current password " })
-    }
-})
-
-const registerUser  = (async (req, res)  => {
-    const {email,password,confirmPassword,name} = req.body;
-    if(password != confirmPassword){
-        throw new ApiError(401,"password and confirm password must be match");
-    }
-        const hash = await encryptPassword(password);
-    
-        const user=await User.findOne({email});
-        if(user){
-            throw new ApiError(401,"user already registerd");
-        }
-   
-        const savedUser = await User.create({
-            email,
-            password:hash,
-            name
-        });
-        const token = await genrateJwtToken({
-            email
-        });
+    const savedUser = await User.create({
+        email,
+        password: hash,
+        name
+    });
+    const token = await genrateJwtToken({
+        email
+    });
     res.status(200).json({
         token: token,
         message: "user is successfully registered",
@@ -60,8 +40,8 @@ const registerUser  = (async (req, res)  => {
 const login = (async (req, res) => {
     const { password, email } = req.body;
     const [user] = await User.find({ email });
-    if(!user){
-       throw new ApiError(401,"user not registered");
+    if (!user) {
+        throw new ApiError(401, "user not registered");
     }
     const result = await bcrypt.compare(password, user?.password);
     if (result) {
@@ -71,7 +51,23 @@ const login = (async (req, res) => {
             token: token
         });
     } else {
-        throw new ApiError( 400,"user id and password is wrong" );
+        throw new ApiError(400, "user id and password is wrong");
+    }
+})
+
+const updatePassword = (async (req, res) => {
+    const { currentPassword, newPassword } = req.body;
+    const { email } = getDecodedToken();
+    const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (isMatch) {
+        const newHashedPassword = await encryptPassword(newPassword);
+        user.password = newHashedPassword;
+        await user.save();
+        res.status(200).json({ message: "Password updated sucessfully" });
+
+    } else {
+        throw new ApiError(401, "current password is wrong");
     }
 })
 
@@ -79,28 +75,22 @@ const genratePasswordResetToken = (async (req, res) => {
     const { email } = req.body;
     const [user] = await User.find({ email });
     if (!user) {
-        res.status(401).json({
-            message: "user email not valid"
-        })
+        throw new ApiError(401,"user email is not valid");
     }
-    const str = crypto.randomBytes(32).toString('hex'); 
-       // Create a hash using SHA-256
-    const hash = crypto.createHash('sha256').update(str).digest('hex');
-    const {origin}=req.headers;
-    console.log(req)
-    const url=`${origin}/auth/reset-password/${hash}`;
+    
     // calculating expiry time
     const now = new Date();
     const expiryTime = new Date(now.getTime() + 30 * 60000);
-    
+
+    const otp=Math.floor(100000 + Math.random() * 900000);
+    user.otp=otp;
+    user.expiryTime=expiryTime;
 
     //saving into db
-    user.passwordResetToken=hash;
-    user.passwordResetTokenExpiry= expiryTime;
     await user.save();
 
     //send mail
-    await emailSend({ from: "jainnaresh1998@gmail.com", to: email, subject: "request for password reset", text: url });
+    await emailSend({ from: "jainnaresh1998@gmail.com", to: email, subject: "request for password reset", text:""+ otp });
 
     // user.passwordResetToken=
     res.status(200).send({
@@ -108,25 +98,25 @@ const genratePasswordResetToken = (async (req, res) => {
     })
 })
 
-const resetPassword= (async (req,res)=>{
-    if(password != confirmPassword){
-        throw new ApiError(401,"password and confirm password must be match");
+const resetPassword = (async (req, res) => {
+    if (password != confirmPassword) {
+        throw new ApiError(401, "password and confirm password must be match");
     }
-    const {password,confirmPassword,resetToken}=req.body;
-    if(!user){
+    const { password, confirmPassword, resetToken } = req.body;
+    if (!user) {
         res.status(401).json({
-            message:"token is not valid"
+            message: "token is not valid"
         })
         return;
     }
-    const {passwordResetTokenExpiry}=user;
+    const { passwordResetTokenExpiry } = user;
     const now = new Date();
     const currentTime = new Date(now.getTime());
     console.log(currentTime)
-    if(currentTime > passwordResetTokenExpiry){
+    if (currentTime > passwordResetTokenExpiry) {
 
         res.status(401).json({
-            message:"token is expire"
+            message: "token is expire"
         })
         return;
     }
@@ -135,13 +125,13 @@ const resetPassword= (async (req,res)=>{
     await user.save();
 
     res.status(200).json({
-        message:"password changed successfully"
+        message: "password changed successfully"
     })
 })
 
-const emailVerify = (async (req,res)=>{
-    
+const emailVerify = (async (req, res) => {
+
 })
 
 
-module.exports = { updatePassword, registerUser, login, genratePasswordResetToken,resetPassword }
+module.exports = { updatePassword, registerUser, login, genratePasswordResetToken, resetPassword }
